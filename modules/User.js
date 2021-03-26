36ee374e5c5d6fe0f3619f4a979ec8a7491af949
Binod Chaudhary <binod@rumsan.com>
2021-03-26 09:12:49 +0545

    add user fields and UI form

--- modules/User.js
+++ modules/User.js
@@ -1,34 +1,51 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    fullname: String,
-    role: String,
-    email: String,
-    password: String,
-    customer: String,
-    schoolid: String,
-    districtid: String,
-    approved: String,
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-    planid: String,
-    notify: Boolean,
-    plan: {
-        item: String,
-        amount: String,
-        invoicelink: String,
-        created: String,
-        status: String,
-        when: String,
-        renewal: Number
+  fullname: String,
+  role: String,
+  email: String,
+  password: String,
+  customer: String,
+  schoolid: String,
+  districtid: String,
+  approved: String,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  planid: String,
+  notify: Boolean,
+  plan: {
+    item: String,
+    amount: String,
+    invoicelink: String,
+    created: String,
+    status: String,
+    when: String,
+    renewal: Number,
+  },
+  extraInfo: {
+    phoneNumber: String,
+    country: String,
+    district: String,
+    city: String,
+    province: String,
+    status: {
+      type: String,
+      enum: ['Current', 'Expired', 'Trial'],
+      default: 'Current',
     },
-    date: {
-        type: Date,
-        default: Date.now
+    expiryDate: Date,
+    reference: String,
+    userType: {
+      type: String,
+      enum: ['District Admin', 'School Admin', 'Home', 'Trial'],
+      default: 'Home',
     },
-})
-
-
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
